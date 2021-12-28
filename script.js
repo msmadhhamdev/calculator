cdb50e4fff70a8f51323f54423c27e4d575ca31f
Adhham Dev <msmadhham@outlook.com>
2021-12-28 20:20:17 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let keys = document.getElementsByClassName("key");
+let input = document.getElementById("input-box");
+for (let k = 0; k < keys.length; k++) {
+    const key = keys[k];
+    key.onclick = () => {
+        if (key.innerHTML == "=") {
+            input.value = eval(input.value);
+        }
+        else if (key.innerHTML == "C") {
+            input.value = "";
+        }
+        else {
+            input.value += key.innerHTML;
+        }
+    }
+}
