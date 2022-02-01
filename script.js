b8e4f4602977ec6ea53b74e55dccfcc7c7e56a89
Adhham Dev <msmadhham@outlook.com>
2022-02-01 19:04:36 +0530

    Format code

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-let keys = document.getElementsByClassName("key");
-let input = document.getElementById("input-box");
+let keys = document.getElementsByClassName('key');
+let input = document.getElementById('input-box');
 for (let k = 0; k < keys.length; k++) {
-    const key = keys[k];
-    key.onclick = () => {
-        if (key.innerHTML == "=") {
-            input.value = eval(input.value);
-        }
-        else if (key.innerHTML == "C") {
-            input.value = "";
-        }
-        else {
-            input.value += key.innerHTML;
-        }
+  const key = keys[k];
+  key.onclick = () => {
+    if (key.innerHTML == '=') {
+      input.value = eval(input.value);
+    } else if (key.innerHTML == 'C') {
+      input.value = '';
+    } else {
+      input.value += key.innerHTML;
     }
+  };
 }
